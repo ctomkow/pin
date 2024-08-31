6e2d9f926830781ed1f7c414cdaf52429755c0e0
Craig Tomkow <ctomkow@gmail.com>
2024-08-30 21:49:13 -0600

    working selector. Had to nuke the table functionality. no way to update the table. Doing it manually works fine

--- pin.go
+++ pin.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	//"github.com/evertras/bubble-table/table"
-	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"io"
@@ -16,23 +14,10 @@ var baseStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.NormalBorder()).
 	BorderForeground(lipgloss.Color("240"))
 
-//const (
-//	columnKeyID     = "id"
-//	columnKeyScore  = "score"
-//	columnKeyStatus = "status"
-//)
-//
-//var (
-//	styleCritical = lipgloss.NewStyle().Foreground(lipgloss.Color("#f00"))
-//	styleStable   = lipgloss.NewStyle().Foreground(lipgloss.Color("#ff0"))
-//	styleGood     = lipgloss.NewStyle().Foreground(lipgloss.Color("#0f0"))
-//)
-
 type model struct {
-	lines   []string
-	columns []table.Column
-	rows    []table.Row
-	table   table.Model
+	lines    []string
+	selected map[int]struct{} // which items are selected
+	cursor   int
 }
 
 func main() {
@@ -71,29 +56,9 @@ func main() {
 }
 
 func newModel(pipedInput string) (m model) {
+
 	m.lines = strings.Split(pipedInput, "\n")
-
-	//tb := table.New(generateColumns(0)).WithRowStyleFunc(rowStyleFunc)
-	//tb.NewColumn("status", "selkd", 3)
-	//tb.NewColumn("diff_data", "your diff. mark the rows you want to keep", 80)
-	m.columns = []table.Column{
-		{Title: "", Width: 1},
-		{Title: "your diff. mark the rows you want to keep", Width: 80},
-	}
-
-	// generate the rows
-	for _, line := range m.lines {
-		row := table.Row{" ", line}
-		m.rows = append(m.rows, row)
-	}
-
-	// Initialize the table model
-	m.table = table.New(
-		table.WithColumns(m.columns),
-		table.WithRows(m.rows),
-		table.WithFocused(true),
-		table.WithHeight(20),
-	)
+	m.selected = make(map[int]struct{})
 
 	return
 }
@@ -108,88 +73,50 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "esc":
-			if m.table.Focused() {
-				m.table.Blur()
-			} else {
-				m.table.Focus()
-			}
-		case "q", "ctrl+c":
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
 			return m, tea.Quit
-		case "enter":
-			selectedRow := m.table.Cursor()
-			m.markTableRowAsSelected(selectedRow)
-			//return m, nil
+			// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.lines)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
 		}
 	}
-	m.table, cmd = m.table.Update(msg)
+	//m.table, cmd = m.table.Update(msg)
 	return m, cmd
 }
 
 func (m model) View() string {
-	// Send the UI for rendering
-	// Highlight the selected row
-	return baseStyle.Render(m.table.View()) + "\n  " + m.table.HelpView() + "\n"
-}
 
-func (m model) markTableRowAsSelected(rowId int) {
-
-	// make new table with the * beside it
-	currentRows := m.table.Rows()
-	m.rows = []table.Row{}
-	m.table = table.Model{}
-
-	for i, row := range currentRows {
-		if i == rowId {
-			row = table.Row{"*", row[1]}
+	s := "Select the differences (-/+) that you want to carry over to the new file\n"
+	s += "------------------------------------------------------------------------\n"
+	for i, line := range m.lines {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
 		}
-		m.rows = append(m.rows, row)
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, line)
+
 	}
-
-	// Initialize the table model
-	m.table = table.New(
-		table.WithColumns(m.columns),
-		table.WithRows(m.rows),
-		table.WithFocused(true),
-		table.WithHeight(20),
-	)
+	return s
 }
-
-//func rowStyleFunc(input table.RowStyleFuncInput) lipgloss.Style {
-//	calculatedStyle := lipgloss.NewStyle()
-//
-//	switch input.Row.Data[columnKeyStatus] {
-//	case "Critical":
-//		calculatedStyle = styleCritical.Copy()
-//	case "Stable":
-//		calculatedStyle = styleStable.Copy()
-//	case "Good":
-//		calculatedStyle = styleGood.Copy()
-//	}
-//
-//	if input.Index%2 == 0 {
-//		calculatedStyle = calculatedStyle.Background(lipgloss.Color("#222"))
-//	} else {
-//		calculatedStyle = calculatedStyle.Background(lipgloss.Color("#444"))
-//	}
-//
-//	return calculatedStyle
-//}
-//
-//func generateColumns(numCritical int) []table.Column {
-//	// Show how many critical there are
-//	statusStr := fmt.Sprintf("Score (%d)", numCritical)
-//	statusCol := table.NewColumn(columnKeyStatus, statusStr, 10)
-//
-//	if numCritical > 3 {
-//		// This normally applies the critical style to everything in the column,
-//		// but in this case we apply a row style which overrides it anyway.
-//		statusCol = statusCol.WithStyle(styleCritical)
-//	}
-//
-//	return []table.Column{
-//		table.NewColumn(columnKeyID, "ID", 10),
-//		table.NewColumn(columnKeyScore, "Score", 8),
-//		statusCol,
-//	}
-//}
